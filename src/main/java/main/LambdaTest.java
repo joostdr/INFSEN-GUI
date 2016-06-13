c6e3a781ab089b015863f4f25a5a4447cc346c8a
Joost de Ruijter <joost1235@hotmail.com>
2016-06-13 23:00:00 +0200

    added basic layout, right row not filled yet

---
+++ src/main/java/main/LambdaTest.java
@@ -0,0 +1,20 @@
+package main;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.function.Consumer;
+
+/**
+ * Created by joost on 13-6-2016.
+ */
+public class LambdaTest {
+
+    public static void main(String[] args) {
+        List<Integer> integerList = Arrays.asList(1,2,3,4,5,6);
+        /*for(int integer : integerList){
+            System.out.println(integer);
+        }*/
+        integerList.forEach((Integer value) -> System.out.println(value));
+    }
+
+}
--- src/main/java/main/MainClass.java
+++ src/main/java/main/MainClass.java
@@ -1,6 +1,9 @@
 package main;
 
+
+import java.awt.*;
 import javax.swing.*;
+import javax.swing.border.Border;
 
 public class MainClass extends JFrame{
 
@@ -8,11 +11,88 @@ public class MainClass extends JFrame{
         initUI();
     }
 
+    private JPanel total;
+    private JPanel leftRow;
+    private JPanel centerRow;
+    private JPanel rightRow;
+    private JPanel addAndReset;
+    private Border border;
+
+
     private void initUI(){
-        setTitle("Example UI Window");
-        setSize(400,400);
+        total = new JPanel();
+        leftRow = createLeftRow();
+        centerRow = createCenterRow();
+        rightRow = createRightRow();
+
+
+        border = BorderFactory.createLineBorder(Color.black);
+        leftRow.setBorder(border);
+        rightRow.setBorder(border);
+        total.setLayout(new BoxLayout(total, BoxLayout.X_AXIS));
+        total.add(leftRow);
+        total.add(centerRow);
+        total.add(rightRow);
+        add(total);
+        setTitle("Boodschappen lijst");
+        setSize(900,500);
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         setLocationRelativeTo(null);
-        setDefaultCloseOperation(EXIT_ON_CLOSE);//
+    }
+
+    private JPanel createLeftRow(){
+        JPanel leftRow = new JPanel();
+        leftRow.setPreferredSize(new Dimension(0,0));
+        leftRow.setLayout(new BoxLayout(leftRow,BoxLayout.Y_AXIS));
+        JPanel dropDownWithLabel = new JPanel();
+        dropDownWithLabel.setLayout(new BoxLayout(dropDownWithLabel, BoxLayout.Y_AXIS));
+        dropDownWithLabel.add(new JLabel("Select a product"));
+        String[] products = {"Banaan", "Appel", "Meme"};
+        JComboBox productList = new JComboBox(products);
+       // productList.setMaximumSize(new Dimension(2000,1000));
+        productList.setSelectedIndex(0);
+        dropDownWithLabel.add(productList);
+        dropDownWithLabel.setPreferredSize(new Dimension(leftRow.getWidth(),4));
+        dropDownWithLabel.setBorder(BorderFactory.createLineBorder(Color.black));
+        dropDownWithLabel.setMaximumSize(new Dimension(3000,3000));
+
+        addAndReset = new JPanel();
+        JButton add = new JButton("add");
+        JButton reset = new JButton("reset");
+        addAndReset.add(add);
+        addAndReset.add(reset);
+
+        leftRow.add(dropDownWithLabel);
+        leftRow.add(addAndReset);
+        leftRow.add(new JLabel("Total price:"));
+
+
+        return leftRow;
+    }
+
+    private JPanel createCenterRow(){
+        JPanel centerRow = new JPanel();
+        centerRow.setPreferredSize(new Dimension(0,0));
+        centerRow.setLayout(new BoxLayout(centerRow,BoxLayout.Y_AXIS));
+        DefaultListModel<String> listModel = new DefaultListModel();
+        listModel.addElement("Jane Doe");
+        listModel.addElement("John Smith");
+        listModel.addElement("Kathy Green");
+        JList<String> list = new JList(listModel);
+        list.setSelectionMode(ListSelectionModel.SINGLE_INTERVAL_SELECTION);
+        list.setLayoutOrientation(JList.VERTICAL);
+        DefaultListCellRenderer renderer = (DefaultListCellRenderer) list.getCellRenderer();
+        renderer.setHorizontalAlignment(SwingConstants.CENTER);
+
+        JScrollPane listScoller = new JScrollPane(list);
+        listScoller.setPreferredSize(new Dimension(250,80));
+        centerRow.add(listScoller);
+        return centerRow;
+    }
+
+    private JPanel createRightRow(){
+        JPanel rightRow = new JPanel();
+        return rightRow;
     }
 
     public static void main(String[] args) {
